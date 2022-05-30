2433cbfd12e66fadb63f00bc033a5edf39464ea4
Encryption-Decryption <titan.ayoub28@gmail.com>
2022-05-30 06:58:03 +0100

    final edit.

--- app/src/main/java/com/example/ds2_sql/MainActivity.java
+++ app/src/main/java/com/example/ds2_sql/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText nom, prix;
@@ -24,11 +26,17 @@ public class MainActivity extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Produit p= new Produit(nom.getText().toString(),Double.parseDouble(prix.getText().toString()));
-                h.insertProduct(p);
+                if (TextUtils.isEmpty(nom.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "vous devez entrer le nom de produit", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(prix.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "vous devez entrer le prix de produit", Toast.LENGTH_SHORT).show();
+                } else {
+                    Produit p = new Produit(nom.getText().toString(), Double.parseDouble(prix.getText().toString()));
+                    h.insertProduct(p);
 
-                Intent i = new Intent(MainActivity.this,ListeProduit.class);
-                startActivity(i);
+                    Intent i = new Intent(MainActivity.this, ListeProduit.class);
+                    startActivity(i);
+                }
             }
         });
     }
